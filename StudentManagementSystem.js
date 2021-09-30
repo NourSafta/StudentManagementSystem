8ab9e9ec3cbe0675577984b3837d44167f447c94
NourSafta <nour.safta@medtech.tn>
2021-09-30 14:49:23 +0200

    first commit

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,48 @@
+var numberOfStudents = 0;
+const students = [];
 
+function addStudent(name)
+{   
+    numberOfStudents += 1;
+    students.push=name;
+    numberOfStudents=students.length ;   
+}
+
+ function getNumberOfStudents ()
+{
+    console.log(numberOfStudents)
+}
+function clearStudents(name)
+{
+    for (let i=0;i< numberOfStudents;i++)
+    {
+        students.pop();
+    }
+}
+function createFullName(firstName,lastName)
+{
+    let studentName= firstName +" "+ lastName;
+    console.log(studentName);
+}
+//return '${firstName} ${lastName}';
+function getStudentByInitials(name,initial)
+{
+    if(studentName)
+    {
+
+    } else {
+
+    }
+}
+
+/*addStudent()
+console.log(numberOfStudents)
+addStudent()
+console.log(numberOfStudents)
+
+
+getNumberOfStudents();
+addStudent();
+getNumberOfStudents();
+
+*/
